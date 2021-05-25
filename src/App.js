85713e8c707c24c5263ec5ed87178fd2e156976f
JUANITOTELO <jdmm.jdmm31@gmail.com>
2021-05-25 13:40:07 -0500

    Linea del tiempo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import wave from "./images/radio.svg"
+import { Chrono } from "react-chrono"
+import grecia from "./images/copa.png"
 
-function App() {
+var images = [grecia];
+
+const Home = () => {
+  const items = [{
+    title: "650 AC",
+    cardTitle: "Teatro Griego",
+    cardDetailedText:
+    [
+      "El teatro griego comenzó en el siglo VI a. C. en Atenas y \
+      fue uno de los primeros expectaculos que se conocen",
+      "La expresion artistica siempre ha sido importante para el bienestar humano, \
+      esta era una de las cosas que los griegos siempre tenian presente."
+    ],
+    media: {
+      type: "IMAGE",
+      source: {
+        url: images[0]
+      },
+    }
+  }
+];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      padding: "2%"
+    }}>
+      <h1 textAlign="center">Linea de tiempo</h1>
+      <Chrono 
+        items={items} 
+        mode="VERTICAL_ALTERNATING" 
+        allowDynamicUpdate="true"
+        theme={{
+          primary: "black",
+          secondary: "#EF9F55", 
+      }}>
+        <div className="chrono-icons">
+          <img style={{width:"5%"}} src={wave} alt="image1" />
+          
+        </div>
+      </Chrono>
+      <div >
+        Icons made by 
+        <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn</a> 
+        from 
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Home;
